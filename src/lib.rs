e86b13c51cc773505a1fcfb311a586a6e74ddcfa
6d7a <westphal@consider-it.de>
2023-11-07 13:55:41 +0100

    chore: initial commit

---
+++ src/lib.rs
@@ -0,0 +1,19 @@
+#![recursion_limit = "32"]
+#![no_std]
+
+mod standards;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
