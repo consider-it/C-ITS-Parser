31a07a625fa1006cfcea547f48207c564997c8ae
6d7a <westphal@consider-it.de>
2024-04-09 09:38:52 +0200

    chore: add asn1 compile script

---
+++ scripts/recompile-asn1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+for path in ../src/standards/asn.1/*.asn; do
+    filename=${path#*1/}
+    filename=${filename%.as*}
+    echo $filename
+    rasn_compiler_cli -m ${path} -o ../src/standards/${filename}.rs
+done
+cargo fmt
