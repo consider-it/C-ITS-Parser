d9d1f90180f58fa21ca34abe19fc86063b94446a
Jannik Beyerstedt <beyerstedt@consider-it.de>
2025-09-19 09:46:12 +0200

    Revert "chore: Upgrade nom to 8.0"
    This reverts commit 5888d3dfc14ae76b617f40bf1672f648b8d49e27.
    
    Our dependencies rely on nom 7 for now (specifically bitvec-nom2 from the
    geonetworking crate). So it doesn't make sense to upgrade here.

--- src/transport/decode.rs
+++ src/transport/decode.rs
@@ -5,7 +5,6 @@ use nom::{
     combinator::{into, map_res},
     error::{ErrorKind, FromExternalError, ParseError},
     sequence::pair,
-    Parser,
 };
 
 #[derive(Debug, PartialEq)]
@@ -55,7 +54,7 @@ pub trait Decode: Sized {
 
 impl Decode for BasicTransportAHeader {
     fn decode(input: &[u8]) -> IResult<&[u8], Self> {
-        into(pair(u16_from_be_bytes, u16_from_be_bytes)).parse(input)
+        into(pair(u16_from_be_bytes, u16_from_be_bytes))(input)
     }
 }
 
@@ -78,7 +77,7 @@ impl BasicTransportAHeader {
 
 impl Decode for BasicTransportBHeader {
     fn decode(input: &[u8]) -> IResult<&[u8], Self> {
-        into(pair(u16_from_be_bytes, u16_from_be_bytes)).parse(input)
+        into(pair(u16_from_be_bytes, u16_from_be_bytes))(input)
     }
 }
 
@@ -106,8 +105,7 @@ fn u16_from_be_bytes(input: &[u8]) -> IResult<&[u8], u16> {
                 "Failed to construct integer from bytes: {e:?}"
             ))
         })
-    })
-    .parse(input)
+    })(input)
 }
 
 impl Decode for IPv6Header {
