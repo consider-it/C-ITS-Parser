c21cab517b4c6b5a60331ad8cd1054b6ddd8abf1
Jannik Beyerstedt <beyerstedt@consider-it.de>
2025-10-15 13:06:15 +0200

    feat: Add conversions to geo-types

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,9 @@ pub mod en;
 #[allow(clippy::all, clippy::pedantic, clippy::nursery, dead_code)]
 pub mod standards;
 
+#[cfg(all(feature = "etsi", feature = "geo"))]
+pub mod geo_utils;
+
 pub(crate) mod pcap;
 pub mod transport;
 
