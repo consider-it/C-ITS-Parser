e1618833a0fac560af8c0c9af9be2c33470e08d8
Jannik Beyerstedt <beyerstedt@consider-it.de>
2026-01-13 14:55:07 +0100

    chore(asn1): Cleanup patch file
    Remove all formatting changes

--- scripts/recompile-asn1.sh
+++ scripts/recompile-asn1.sh
@@ -1,14 +1,24 @@
 #!/bin/bash
+echo "generating code from ASN.1 sources..."
 for path in src/standards/asn.1/*.asn; do
     filename=${path#*1/}
     filename=${filename%.as*}
     echo $filename
     rasn_compiler_cli -m ${path} -o src/standards/${filename}.rs
 done
+
+echo ""
+echo "applying patches..."
 for file in src/standards/*.rs; do
     rustfmt +nightly ${file}
 done
 git apply scripts/asn1.patch
+
+echo ""
+echo "formatting code..."
 for file in src/standards/*.rs; do
     rustfmt +nightly ${file}
 done
+
+echo ""
+echo "done!"
