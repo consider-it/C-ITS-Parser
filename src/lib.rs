d20cdc896f0df23898193b184a22e197fbc81830
Jannik Beyerstedt <beyerstedt@consider-it.de>
2026-05-21 13:24:59 +0200

    docs: Replace deprecated `doc_auto_cfg` feature with `doc_cfg`

--- src/lib.rs
+++ src/lib.rs
@@ -51,7 +51,7 @@
 //! - `geo`: Enable conversions to [geo-types](https://crates.io/crates/geo-types) (as lon/lat coordinates in degrees)
 
 #![cfg_attr(not(target_arch = "wasm32"), no_std)]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 
 extern crate alloc;
 
